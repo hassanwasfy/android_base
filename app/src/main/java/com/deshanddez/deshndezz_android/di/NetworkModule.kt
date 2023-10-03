c80f28ca248bffc85b13c275ae96a0845fb0096f
abaferas <wasfyelgamed@gmail.com>
2023-10-03 16:11:50 +0300

    init hilt dependency injection

---
+++ app/src/main/java/com/deshanddez/deshndezz_android/di/NetworkModule.kt
@@ -0,0 +1,12 @@
+package com.deshanddez.deshndezz_android.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    
+}
